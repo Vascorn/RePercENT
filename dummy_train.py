2252346cef121883f3a60634b9c686d3e10b9884
Vasiliki Rizou <vasiliki.rizou@epfl.ch>
2025-11-21 16:30:02 +0100

    no dockerfile?

--- dummy_train.py
+++ dummy_train.py
@@ -5,6 +5,7 @@ def train(epochs= 5):
     for epoch in range(epochs):
         # renerate radom loss values
         loss = np.random.rand()
+        # wandb.log({"loss": loss})
         print(f"Epoch {epoch + 1} / {epochs}, Loss: {loss:.4f}")
 
 
