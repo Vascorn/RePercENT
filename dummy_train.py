5f8dfccb9161fedd16f29fec38522ca12c631658
Vasiliki Rizou <vasiliki.rizou@epfl.ch>
2025-11-24 16:43:27 +0100

    testing wandb

--- dummy_train.py
+++ dummy_train.py
@@ -2,15 +2,21 @@ import numpy as np
 import wandb
 import time
 
+run = wandb.init(project= "dummy-training", 
+                 comment= "testing wandb logging",
+                 mode= "online",
+                 config= {
+                     "epochs": 10
+                 })
+
 def train(epochs= 5):
     for epoch in range(epochs):
         # renerate radom loss values
         loss = np.random.rand()
-        # wandb.log({"loss": loss})
-        # pause for 30 seconds
-        time.sleep(100)
-        print(f"Epoch {epoch + 1} / {epochs}, Loss: {loss:.4f}")
+        
+        wandb.log({"epoch": epoch, "loss": loss})
 
 
 if __name__ == "__main__":
-    train()
+    epochs = run.config.epochs
+    train(epochs)
