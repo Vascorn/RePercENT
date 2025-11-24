429e0889a50184355719ddcdcca7b4cd7dcb58f5
Vasiliki Rizou <vasiliki.rizou@epfl.ch>
2025-11-24 16:25:01 +0100

    imported time

--- dummy_train.py
+++ dummy_train.py
@@ -1,12 +1,14 @@
 import numpy as np
 import wandb
-
+import time
 
 def train(epochs= 5):
     for epoch in range(epochs):
         # renerate radom loss values
         loss = np.random.rand()
         # wandb.log({"loss": loss})
+        # pause for 30 seconds
+        time.sleep(30)
         print(f"Epoch {epoch + 1} / {epochs}, Loss: {loss:.4f}")
 
 
